a2cd26fbf24f72bc677dee7f5246edbcf81705ab
animesh sahoo <animeshsahoo159@gmail.com>
2025-06-08 03:52:59 +0530

    added get hospital details route

--- backend/src/routes/hospital.route.js
+++ backend/src/routes/hospital.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import  authorizeRole  from "../middlewares/authorizeRole.middleware.js";
-import { createHospital, deleteHospital, getAllHospitals, getHospitalById, updateHospital } from "../controllers/hospital.controller.js";
+import { createHospital, deleteHospital, getAllHospitals, getHospitalById, getHospitalDetails, updateHospital } from "../controllers/hospital.controller.js";
 
 const router= Router()
 
@@ -11,6 +11,7 @@ router.route("/:hospitalId").get( getHospitalById)
 
 //authorized routes
 router.route("/create-hospital").post(verifyJWT,authorizeRole("hospital"), createHospital)
+router.route("/get-hospital").post(verifyJWT,authorizeRole("hospital"), getHospitalDetails)
 router.route("/update-hospital").patch(verifyJWT,authorizeRole("hospital"), updateHospital)
 router.route("/delete-hospital").get(verifyJWT,authorizeRole("hospital"), deleteHospital)
 
