3ff1f02b08bd9a0f4139b5adbec80c5804939d22
animesh sahoo <animeshsahoo159@gmail.com>
2025-06-10 01:17:44 +0530

    populate doctor user

--- backend/src/controllers/patient.controller.js
+++ backend/src/controllers/patient.controller.js
@@ -8,54 +8,70 @@ import { isValidObjectId } from "mongoose";
 
 //!AUTHORIZE ROLE MIDDLEWARE ALREADY CHECKS IF ROLE IS PATIENT SO WE DONT HAVE TO CHECK IT IN ANY OF CONTROLLERS
 
-const createPatient=asyncHandler(async(req,res)=>{
-    const {gender,dob}=req.body
-    const userId=req.user._id;
+const createPatient = asyncHandler(async (req, res) => {
+  const { gender, dob } = req.body;
+  const userId = req.user._id;
 
-    if(!gender.trim() || !dob){
-        throw new ApiError(400,"all fields are required");
-    }
+  if (!gender.trim() || !dob) {
+    throw new ApiError(400, "all fields are required");
+  }
 
-    const patient=await Patient.create(
-        {
-            user: userId,
-            gender,
-            dob
-        }
-    )
+  const patient = await Patient.create({
+    user: userId,
+    gender,
+    dob,
+  });
 
-    const createdPatient = await Patient.findById(patient._id)
-    if (!createdPatient) {
-        throw new ApiError(500, "Something went wrong while making patient profile")
-    }
+  const createdPatient = await Patient.findById(patient._id);
+  if (!createdPatient) {
+    throw new ApiError(
+      500,
+      "Something went wrong while making patient profile"
+    );
+  }
 
-    return res.status(201).json(
-        new ApiResponse(200, createdPatient, "patient profile created succesfully")
-    )
-
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        200,
+        createdPatient,
+        "patient profile created succesfully"
+      )
+    );
 });
 
 const getPatientDetails = asyncHandler(async (req, res) => {
-    const userId = req.user._id;
+  const userId = req.user._id;
 
-    const patient = await Patient.findOne({ user: userId }).populate("user", "-password -refreshToken");
+  const patient = await Patient.findOne({ user: userId }).populate(
+    "user",
+    "-password -refreshToken"
+  );
 
-    if (!patient) {
-      throw new ApiError(404, "Patient profile not found");
-    }
+  if (!patient) {
+    throw new ApiError(404, "Patient profile not found");
+  }
 
-    res.status(200).json(new ApiResponse(200, patient, "Patient profile fetched successfully"));
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, patient, "Patient profile fetched successfully")
+    );
 });
 
 const getAppointmentsForPatient = asyncHandler(async (req, res) => {
   const userId = req.user._id;
 
-  const patient = await Patient.findOne({ user: userId }).populate("user", "-password -refreshToken");
+  const patient = await Patient.findOne({ user: userId }).populate(
+    "user",
+    "-password -refreshToken"
+  );
 
   if (!patient) {
     throw new ApiError(404, "Patient profile not found");
   }
-  const patientId=patient._id
+  const patientId = patient._id;
   const { status } = req.query;
 
   if (!isValidObjectId(patientId)) {
@@ -68,21 +84,35 @@ const getAppointmentsForPatient = asyncHandler(async (req, res) => {
   }
 
   const appointments = await Appointment.find(query)
-    .populate("doctor", "specialization experience")
+    .populate({
+      path: "doctor",
+      select: "specialization experience user",
+      populate: {
+        path: "user",
+        select: "fullName avatar email",
+      },
+    })
     .sort({ createdAt: -1 });
 
-  res.status(200).json(new ApiResponse(200, appointments, "Appointments fetched successfully"));
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, appointments, "Appointments fetched successfully")
+    );
 });
 
 const deletePatient = asyncHandler(async (req, res) => {
   const userId = req.user._id;
 
-  const findPatient = await Patient.findOne({ user: userId }).populate("user", "-password -refreshToken");
+  const findPatient = await Patient.findOne({ user: userId }).populate(
+    "user",
+    "-password -refreshToken"
+  );
 
   if (!findPatient) {
     throw new ApiError(404, "Patient profile not found");
   }
-  const patientId=findPatient._id
+  const patientId = findPatient._id;
 
   const patient = await Hospital.findById(patientId);
   if (!patient) {
@@ -94,35 +124,41 @@ const deletePatient = asyncHandler(async (req, res) => {
     throw new ApiError(403, "Unauthorized to delete this hospital");
   }
 
-  const deletePatient=await Patient.deleteOne(patient._id);
-   if(deletePatient.deletedCount===0){
-        throw new ApiError(500, "unable to delete hospital")
-    }
+  const deletePatient = await Patient.deleteOne(patient._id);
+  if (deletePatient.deletedCount === 0) {
+    throw new ApiError(500, "unable to delete hospital");
+  }
 
-    res.status(200).json(new ApiResponse(200, deleteHospital, "hospital deleted successfully"))
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, deleteHospital, "hospital deleted successfully")
+    );
 });
 
 const getNearbyHospitals = asyncHandler(async (req, res) => {
-    const { lat, lon, maxDistance = 5000 } = req.query; //lat lon will be sent from frontend using geolocation API in browser
+  const { lat, lon, maxDistance = 5000 } = req.query; //lat lon will be sent from frontend using geolocation API in browser
 
-    if (!lat || !lon) {
-        throw new ApiError(400, "Latitude and longitude are required");
-    }
+  if (!lat || !lon) {
+    throw new ApiError(400, "Latitude and longitude are required");
+  }
 
-    const hospitals = await Hospital.find({
-        location: {
-            $near: {
-                $geometry: {
-                    type: "Point",
-                    coordinates: [parseFloat(lon), parseFloat(lat)]
-                },
-                $maxDistance: parseFloat(maxDistance) // optional: filter by radius
-            }
-        }
-    });
+  const hospitals = await Hospital.find({
+    location: {
+      $near: {
+        $geometry: {
+          type: "Point",
+          coordinates: [parseFloat(lon), parseFloat(lat)],
+        },
+        $maxDistance: parseFloat(maxDistance), // optional: filter by radius
+      },
+    },
+  });
 
-    return res.status(200).json(
-        new ApiResponse(200, hospitals, "Nearby hospitals fetched successfully")
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, hospitals, "Nearby hospitals fetched successfully")
     );
 });
 
@@ -154,15 +190,16 @@ const updatePatient = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Failed to update patient info");
   }
 
-  res.status(200).json(new ApiResponse(200, updatedPatient, "Updated patient successfully"));
+  res
+    .status(200)
+    .json(new ApiResponse(200, updatedPatient, "Updated patient successfully"));
 });
 
-
 export {
-    createPatient,
-    getPatientDetails,
-    getAppointmentsForPatient,
-    deletePatient,
-    getNearbyHospitals,
-    updatePatient
-}
+  createPatient,
+  getPatientDetails,
+  getAppointmentsForPatient,
+  deletePatient,
+  getNearbyHospitals,
+  updatePatient,
+};
